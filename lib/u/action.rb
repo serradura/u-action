bb8d384695b96e33afe7f836fe1e2115cd206884
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-12-17 14:34:42 -0300

    First commit

---
+++ lib/u/action.rb
@@ -0,0 +1,8 @@
+require "u/action/version"
+
+module U
+  module Action
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/u/action/version.rb
@@ -0,0 +1,5 @@
+module U
+  module Action
+    VERSION = "0.0.0"
+  end
+end
